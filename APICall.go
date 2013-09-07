e6909453d77d37aa1884c8aae10eb34d8ae4d8d2
Liam Zdenek <liam@vubeology.com>
2013-09-07 13:08:06 -0500

    Implementing case insensitive methods

--- APICall.go
+++ APICall.go
@@ -1,16 +1,18 @@
 package gojr
 
+import "strings"
+
 type APICall struct {
 	Method string
 	Function HTTPFunc
 }
 
 func NewAPICall(method string, function HTTPFunc) *APICall {
-	return &APICall{Method: method, Function: function}
+	return &APICall{Method: strings.ToUpper(method), Function: function}
 }
 
 func (c *APICall) Step(req *Request, url string) (bool, interface{}) {
-	if (len(url) == 0 || url == "/") && c.Method == req.Method {
+	if (len(url) == 0 || url == "/") && c.Method == strings.ToUpper(req.Method) /* c.Method is already guaranteed to be upper case */ {
 		return true, c.Function(req)
 	}
 	return false, nil
